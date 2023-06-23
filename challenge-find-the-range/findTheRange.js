929d17645799f9ef1d5f9998790d77bdd147aebd
Kristen Moore <kookwithkris@yahoo.com>
2023-06-23 13:08:22 -0400

    initial commit

---
+++ challenge-find-the-range/findTheRange.js
@@ -0,0 +1,10 @@
+/* Given an array of numbers, write some code to loop through the array,
+and print out the smallest and largest numbers in the array. */
+
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+
+console.log(Math.min(...array)) 
+console.log(Math.max(...array)) 
